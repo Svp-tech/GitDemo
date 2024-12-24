c9069371e2294aff47d328444ba2124d4349540f
sony <pswati740@gmail.com>
2024-12-24 22:53:21 +0530

    its commit by gitstuff

---
+++ src/test/java/rahulshettyacademy/Tests/GitTrial.java
@@ -0,0 +1,10 @@
+package rahulshettyacademy.Tests;
+
+public class GitTrial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I am new change by gitstuff");
+	}
+
+}
